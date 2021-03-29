51174951f2df69649825ae8ee6a19c2edd3536b2
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-29 11:12:07 -0700

    display array of flavors, option for user to add more
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
+const myFavs=["chocolate", "cherry", "mint"];
 
+$(document).ready(function(){
+  myFavs.forEach(function(fav){
+    $("#fav").append("<li>"+fav+"</li>");
+  });
+  $("#new-flavor").submit(function(event){
+    event.preventDefault();
+    $("#fav").append("<li>"+($("#new").val())+"</li>");
+  });
+});
